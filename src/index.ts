67a9db2d14fe34ff9cb952b6c11cd278054539e0
hg-ian-choi <hg.ian.choi@gmail.com>
2022-10-21 01:59:02 +0900

    try one-to-one relation

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { AppDataSource } from './data-source';
 import { Photo } from './entity/Photo';
+import { PhotoMetadata } from './entity/PhotoMetadata';
 import { User } from './entity/User';
 
 AppDataSource.initialize()
@@ -22,64 +23,97 @@ AppDataSource.initialize()
 
     // Creating and inserting a photo into the database
     console.log('Inserting a new photo into the database...');
-    const photo = new Photo();
-    photo.name = 'Me and Bears';
-    photo.description = 'I am near polar bears';
-    photo.filename = 'photo-with-bears.jpg';
-    photo.views = 1;
-    photo.isPublished = true;
+    const photo1 = new Photo();
+    photo1.name = 'Me and Bears';
+    photo1.description = 'I am near polar bears';
+    photo1.filename = 'photo-with-bears.jpg';
+    photo1.views = 1;
+    photo1.isPublished = true;
 
-    await AppDataSource.manager.save(photo);
-    console.log('Photo has been saved. Photo id is', photo.id);
+    await AppDataSource.manager.save(photo1);
+    console.log('Photo has been saved. Photo id is', photo1.id);
 
     // Using Entity Manager
     const savedPhotos = await AppDataSource.manager.find(Photo);
     console.log('All photos from the db: ', savedPhotos);
 
-    // Using Repositories
+    // Using2 Repositories
     const photo2 = new Photo();
-    photo2.name = 'Me and Bears';
+    photo2.name2 = 'Me and Bears';
     photo2.description = 'I am near polar bears';
     photo2.filename = 'photo-with-bears.jpg';
     photo2.views = 1;
-    photo2.isPublished = true;
+    photo2.isPublished = true;2
 
-    const photoRepository = AppDataSource.getRepository(Photo);
+    const photoRepository2 = AppDataSource.getRepository(Photo);
 
-    await photoRepository.save(photo2);
+    await photoRepository2.save(photo2);2
     console.log('Photo has been saved');
 
     // Loading from the database
-    const savedPhotos2 = await photoRepository.find();
+    const savedPhotos2 = await photoRepository2.find();
     console.log('All photos from the db: ', savedPhotos2);
 
-    const firstPhoto = await photoRepository.findOneBy({ id: 1 });
+    const firstPhoto = await photoRepository2.findOneBy({ id: 1 });
     console.log('First photo from the db: ', firstPhoto);
 
-    const meAndBearsPhoto = await photoRepository.findOneBy({
+    const meAndBearsPhoto = await photoRepository2.findOneBy({
       name: 'Me and Bears',
     });
     console.log('Me and Bears photo from the db: ', meAndBearsPhoto);
 
-    const allViewedPhotos = await photoRepository.findBy({ views: 1 });
+    const allViewedPhotos = await photoRepository2.findBy({ views: 1 });
     console.log('All viewed photos: ', allViewedPhotos);
 
-    const allPublishedPhotos = await photoRepository.findBy({
+    const allPublishedPhotos = await photoRepository2.findBy({
       isPublished: true,
     });
     console.log('All published photos: ', allPublishedPhotos);
 
-    const [photos, photosCount] = await photoRepository.findAndCount();
+    const [photos, photosCount] = await photoRepository2.findAndCount();
     console.log('All photos: ', photos);
     console.log('Photos count: ', photosCount);
 
     // Updating in the database
-    const photoToUpdate = await photoRepository.findOneBy({ id: 1 });
+    const photoToUpdate = await photoRepository2.findOneBy({ id: 1 });
     photoToUpdate.name = 'Me, my friends and polar bears';
-    await photoRepository.save(photoToUpdate);
+    await photoRepository2.save(photoToUpdate);
 
     // Removing from the database
-    const photoToRemove = await photoRepository.findOneBy({ id: 1 });
-    await photoRepository.remove(photoToRemove);
+    const photoToRemove = await photoRepository2.findOneBy({ id: 1 });
+    await photoRepository2.remove(photoToRemove);
+
+    // Save a one-to-one relation
+    // create a photo
+    const photo3 = new Photo();
+    photo3.name = 'Me and Bears';
+    photo3.description = 'I am near polar bears';
+    photo3.filename = 'photo-with-bears.jpg';
+    photo3.views = 1;
+    photo3.isPublished = true;
+
+    // create a photo metadata
+    const metadata = new PhotoMetadata();
+    metadata.height = 640;
+    metadata.width = 480;
+    metadata.compressed = true;
+    metadata.comment = 'cybershoot';
+    metadata.orientation = 'portrait';
+    metadata.photo = photo3; // this way we connect them
+
+    // get entity repositories
+    const photoRepository3 = AppDataSource.getRepository(Photo);
+    const metadataRepository = AppDataSource.getRepository(PhotoMetadata);
+
+    // first we should save a photo
+    await photoRepository3.save(photo);
+
+    // photo is saved. Now we need to save a photo metadata
+    await metadataRepository.save(metadata);
+
+    // done
+    console.log(
+      'Metadata is saved, and the relation between metadata and photo is created in the database too'
+    );
   })
   .catch((error) => console.log(error));
